ee5af420b1f2daa3e601641a2b52dab4497292fb
Bogus <bmalyk7@gmail.com>
2021-02-14 15:57:35 +0100

    next two commit

--- scripts/script.js
+++ scripts/script.js
@@ -2,6 +2,7 @@
 
 const hamburger = document.querySelector('.hamburger');
 const menu = document.querySelector('.menu');
+const navMenu = document.querySelector('.nav_menu');
 const hamburgerArr = [hamburger.querySelector('.top'),hamburger.querySelector('.middle'),hamburger.querySelector('.bottom')];
 
 
@@ -10,8 +11,10 @@ const hamburgerArr = [hamburger.querySelector('.top'),hamburger.querySelector('.
 
 menu.addEventListener('click',(e)=>{
     const target = e.target;
-    if(target&&target.classList.contains('hamburger')){
+    if(target&&target.classList.contains('check')){
         hamburgerArr.forEach(item=>item.classList.toggle('active'));
+        navMenu.classList.toggle('active');
+        document.querySelector('body').classList.toggle('hidden');
     }
 })
 
